643f2b51a862b88108bb4022595df2a25aa091fe
hbjORbj <sldisek783@gmail.com>
2020-12-19 08:47:18 +0000

    Added fields to Post Schema

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
 const PostSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -8,6 +10,19 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  photo: {
+    data: Buffer,
+    contentType: String,
+  },
+  postedBy: {
+    type: ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
 });
 
 module.exports = mongoose.model("Post", PostSchema);
